34626dc810cdcd2d1f96fe9a7a3a25005fceea9c
Ao Dong <53056165+Ao99@users.noreply.github.com>
2021-05-10 20:55:23 -0400

    change to static

--- lib/services/emotionService.dart
+++ lib/services/emotionService.dart
@@ -2,18 +2,18 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import '../models/emotion.dart';
 
 class EmotionService {
-  final emotionRef = FirebaseFirestore.instance
+  static final emotionRef = FirebaseFirestore.instance
     .collection('emotions')
     .withConverter(
       fromFirestore: (snapshots,_) => Emotion.fromJson(snapshots.data()),
       toFirestore: (emotion,_) => emotion.toJson(),
     );
   
-  Future<QuerySnapshot<Emotion>> getAllEmotions() {
+  static Future<QuerySnapshot<Emotion>> getAllEmotions() {
     return emotionRef.orderBy('id').get();
   }
 
-  Future<DocumentSnapshot<Emotion>> getEmotionByDocId(String docId) {
+  static Future<DocumentSnapshot<Emotion>> getEmotionByDocId(String docId) {
     return emotionRef.doc(docId).get();
   }
 
