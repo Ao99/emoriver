e4f23f2c2237d7105dde5d014a2f1c2cb4619630
Ao Dong <53056165+Ao99@users.noreply.github.com>
2021-05-10 21:20:17 -0400

    Update overviewView.dart

--- lib/pages/tabs/overviewView.dart
+++ lib/pages/tabs/overviewView.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import '../../utils/colors.dart';
-import '../../widgets/circleButton.dart';
 
 class OverviewView extends StatelessWidget {
   final IconData tabIcon = Icons.date_range;
@@ -9,12 +7,7 @@ class OverviewView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child:
-        CircleButton(
-          color: ThemeColors.happy,
-          onTap: () {print('happy');},
-          child: Text("Happy"),
-        ),
+        child: Icon(tabIcon),
       ),
     );
   }
