2ad59b667cbcff79d9c582ac3118021a99fa3c40
Ao Dong <53056165+Ao99@users.noreply.github.com>
2021-05-10 21:19:10 -0400

    Update loginPage.dart

--- lib/pages/loginPage.dart
+++ lib/pages/loginPage.dart
@@ -9,32 +9,7 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Hero(
-          tag: 'hero-add',
-          child: _createHeroContainer(
-            size: 100.0,
-            color: Colors.white,
-          ),
-        )
-      ),
+      body: Container(),
     );
   }
-}
-
-StatelessWidget _createHeroContainer({
-  double size,
-  Color color,
-}) {
-  return Container(
-    height: size,
-    width: size,
-    padding: EdgeInsets.all(10.0),
-    margin: size < 100.0 ? EdgeInsets.all(10.0) : EdgeInsets.all(0),
-    decoration: BoxDecoration(
-      shape: BoxShape.circle,
-      color: color,
-    ),
-    child: FlutterLogo(),
-  );
 }
