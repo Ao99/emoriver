5cffcb876456c51ebc91b46b89ef4c410d0080ad
Ao Dong <53056165+Ao99@users.noreply.github.com>
2021-05-13 17:18:15 -0400

    Create location.dart

---
+++ lib/utils/location.dart
@@ -0,0 +1,25 @@
+import 'package:location/location.dart';
+
+Future<LocationData> getLocation() async {
+  Location location = Location();
+  bool _serviceEnabled;
+  PermissionStatus _permissionGranted;
+
+  _serviceEnabled = await location.serviceEnabled();
+  if (!_serviceEnabled) {
+    _serviceEnabled = await location.requestService();
+    if (!_serviceEnabled) {
+      return Future.error('Location services are disabled.');
+    }
+  }
+
+  _permissionGranted = await location.hasPermission();
+  if (_permissionGranted == PermissionStatus.denied) {
+    _permissionGranted = await location.requestPermission();
+    if (_permissionGranted != PermissionStatus.granted) {
+      return Future.error('Location permissions are denied');
+    }
+  }
+
+  return await location.getLocation();
+}
